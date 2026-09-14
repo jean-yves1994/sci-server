5e56ac8d7a77c9abbd378593780c69d32e3f4f58
jean-yves1994 <55282546+jean-yves1994@users.noreply.github.com>
2026-09-14 15:49:51 +0000

    Revert unnecessary service module coupling

--- src/inspections/inspections.module.ts
+++ src/inspections/inspections.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ReportsModule } from '../reports/reports.module';
 import { InspectionsController } from './inspections.controller';
 import { InspectionsService } from './inspections.service';
 
 @Module({
-  imports: [ReportsModule],
   controllers: [InspectionsController],
   providers: [InspectionsService],
   exports: [InspectionsService],
