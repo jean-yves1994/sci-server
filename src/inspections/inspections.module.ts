4d4917c3cf65222a0fc43735e9e835f48f1f2d4a
jean-yves1994 <55282546+jean-yves1994@users.noreply.github.com>
2026-09-14 15:50:01 +0000

    Import reports module for submission draft generation

--- src/inspections/inspections.module.ts
+++ src/inspections/inspections.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ReportsModule } from '../reports/reports.module';
 import { InspectionsController } from './inspections.controller';
 import { InspectionsService } from './inspections.service';
 
 @Module({
+  imports: [ReportsModule],
   controllers: [InspectionsController],
   providers: [InspectionsService],
   exports: [InspectionsService],
