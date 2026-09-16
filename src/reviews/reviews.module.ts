348f4fa91ba48ad07b937317c3134a622ae22d98
jean-yves1994 <55282546+jean-yves1994@users.noreply.github.com>
2026-09-16 09:43:11 +0000

    Register admin inspection editor

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { InspectionsModule } from '../inspections/inspections.module';
 import { ReportsModule } from '../reports/reports.module';
+import { AdminInspectionEditController } from './admin-inspection-edit.controller';
+import { AdminInspectionEditService } from './admin-inspection-edit.service';
 import { ReviewerMapController } from './reviewer-map.controller';
 import { ReviewerMapService } from './reviewer-map.service';
 import { ReviewsController } from './reviews.controller';
@@ -8,7 +10,7 @@ import { ReviewsService } from './reviews.service';
 
 @Module({
   imports: [InspectionsModule, ReportsModule],
-  controllers: [ReviewsController, ReviewerMapController],
-  providers: [ReviewsService, ReviewerMapService],
+  controllers: [ReviewsController, ReviewerMapController, AdminInspectionEditController],
+  providers: [ReviewsService, ReviewerMapService, AdminInspectionEditService],
 })
 export class ReviewsModule {}
