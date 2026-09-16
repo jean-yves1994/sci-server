8ce5c74941ca927d1e244a2e0e13625cddee22fb
jean-yves1994 <55282546+jean-yves1994@users.noreply.github.com>
2026-09-16 09:02:49 +0000

    Register reviewer map components

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { InspectionsModule } from '../inspections/inspections.module';
 import { ReportsModule } from '../reports/reports.module';
+import { ReviewerMapController } from './reviewer-map.controller';
+import { ReviewerMapService } from './reviewer-map.service';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
 
 @Module({
   imports: [InspectionsModule, ReportsModule],
-  controllers: [ReviewsController],
-  providers: [ReviewsService],
+  controllers: [ReviewsController, ReviewerMapController],
+  providers: [ReviewsService, ReviewerMapService],
 })
 export class ReviewsModule {}
